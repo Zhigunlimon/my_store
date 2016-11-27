51815c32ca0b4d5dc6d845ecd5330684794fd5e2
VanyaLiashuk <ivan129522@gmail.com>
2016-11-27 17:33:45 +0200

    Initialize repository

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,39 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+  # /items/1 GET
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: "Page not found", status: 404
+    end
+  end
+
+  # /items/new  GET
+  def new
+  end
+
+  # /items/edit GET
+  def edit
+  end
+
+  # /items  POST
+  def create
+    @item = Item.create(params[:item])
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render "new"
+    end
+  end
+
+  # /items/1 PUT
+  def update
+  end
+
+  # /items/1  DELETE
+  def destroy
+  end
+
+end
